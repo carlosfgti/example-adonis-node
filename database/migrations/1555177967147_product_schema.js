9ac2c0fef54a473c46159b2aef81428d6a5b49b9
Carlos Ferreira <conta@especializati.com.br>
2019-04-13 14:53:03 -0300

    Create model and migration file

---
+++ database/migrations/1555177967147_product_schema.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProductSchema extends Schema {
+  up () {
+    this.create('products', (table) => {
+      table.increments()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('products')
+  }
+}
+
+module.exports = ProductSchema
