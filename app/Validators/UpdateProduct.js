5bbb7414b4490768ba8096a472bd29d74d43d902
Carlos Ferreira <conta@especializati.com.br>
2019-04-29 15:00:07 -0300

    Validation Custom Messages

--- app/Validators/UpdateProduct.js
+++ app/Validators/UpdateProduct.js
@@ -9,6 +9,19 @@ class UpdateProduct {
       description: 'required|min:3|max:100',
     }
   }
+
+  get messages () {
+    return {
+      'title.required': 'Precisa informar o título',
+      'title.min': 'Quantidade mínima de 3 caracteres',
+      'title.max': 'Quantidade mínima de 100 caracteres',
+      'title.unique': 'O título deve ser único',
+
+      'description.required': 'A descrição deve ser informado',
+      'description.min': 'Quantidade mínima de 3 caracteres',
+      'description.max': 'Quantidade mínima de 100 caracteres',
+    }
+  }
 }
 
 module.exports = UpdateProduct
